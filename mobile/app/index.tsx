ef6bc886f18ce2ccae66ab76a57049cf6b471f65
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2025-12-14 06:27:32 +0800

    feat: enhance layout with SafeScreen and update index styles

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,28 +1,22 @@
-import { Image } from "expo-image";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/about"}>About</Link>
-      {/* <Image
-        source={{
-          uri: "https://media.istockphoto.com/id/2211409841/photo/typing-laptop.webp?a=1&b=1&s=612x612&w=0&k=20&c=LaVJ67eEunNDxVGs7oiRqQzPR1AUKH3KN5j8nkWbwtE=",
-        }}
-        style={{ width: 100, height: 100 }}
-      /> */}
-      <Image
-        source={require("../assets/images/react-logo.png")}
-        style={{ width: 100, height: 100 }}
-      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "purple",
+  },
+  heading: {
+    fontSize: 48,
+    color: "blue",
+  },
+});
---
+++ mobile/components/SafeScreen.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, View } from "react-native";
+import React, { ReactNode } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { COLORS } from "../constants/colors";
+
+type SafeScreenProps = {
+  children: ReactNode;
+};
+
+const SafeScreen = ({ children }: SafeScreenProps) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { paddingTop: insets.top, backgroundColor: COLORS.background },
+      ]}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default SafeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
