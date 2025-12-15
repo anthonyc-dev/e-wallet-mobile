681e6d7bdd022a64e3af53ce7eb3e6ba70029500
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2025-12-15 20:12:20 +0800

    feat: update app slug to 'Savey', implement splash screen options with fade animation, and modify splash icon image

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,6 +3,13 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Slot } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   return (
